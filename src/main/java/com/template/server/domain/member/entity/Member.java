e20410626275483d5844ab9a4b3bd052ac533e07
Gonue <6017891@naver.com>
2023-06-06 13:48:11 +0900

    fix : member entity

--- src/main/java/com/template/server/domain/member/entity/Member.java
+++ src/main/java/com/template/server/domain/member/entity/Member.java
@@ -1,6 +1,7 @@
 package com.template.server.domain.member.entity;
 
 import com.template.server.global.audit.AuditingFields;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,18 +10,24 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Setter
+@Table(name = "member")
 @Entity
 public class Member extends AuditingFields {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", updatable = false)
     private Long memberId;
+    @Column(name = "email", nullable = false, unique = true, updatable = false, length = 50)
     private String email;
+    @Column(name = "nick_name", nullable = false)
     private String nickName;
+    @Column(name = "password", nullable = false)
     private String password;
+    @Column(name = "profile_image")
     private String profileImage;
 
     @ElementCollection(fetch = FetchType.EAGER)
